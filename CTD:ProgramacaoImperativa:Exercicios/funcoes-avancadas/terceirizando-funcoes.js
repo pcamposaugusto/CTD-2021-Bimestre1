1d9300bfda68e0f731c0cb745c0fb3f427095769
pcamposaugusto <paulaocaugusto@gmail.com>
2021-06-14 15:30:53 -0300

    add exercícios de funções avançadas

--- CTD:ProgramacaoImperativa:Exercicios/funcoes-avancadas/terceirizando-funcoes.js
+++ CTD:ProgramacaoImperativa:Exercicios/funcoes-avancadas/terceirizando-funcoes.js
@@ -1,8 +1,8 @@
-const acaoCarro = (callback) => callback;
+const acaoCarro = (callback) => callback();
 
 const andar = () => console.log("O carro está andando.");
 
 const parar = () => console.log("O carro parou.");
 
-acaoCarro(andar());
-acaoCarro(parar());
+acaoCarro(andar);
+acaoCarro(parar);
